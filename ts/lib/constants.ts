d6dfed7de9404be2e1f07d6070004831658398ba
Alan Wernick <Alan.Wernick@bmwna.com>
2017-06-08 14:23:14 -0700

    Refactor interfaces and constants

---
+++ ts/lib/constants.ts
@@ -0,0 +1,9 @@
+// Enums
+export enum State {
+  IN_PROGRESS = 0,
+  FINISHED =  1
+}
+
+export enum Event {
+  END_OF_UTTERANCE = 1
+}
--- ts/lib/google-assistant.ts
+++ ts/lib/google-assistant.ts
@@ -1,49 +1,14 @@
 import * as stream from "stream";
+import { State, Event } from "./constants";
+import { AudioInOptions , AudioOutOptions }  from "./options";
+import { 
+  ConverseConfig, AudioInConfig, 
+  AudioOutConfig, AssistantConfig } from "./config";
 
 let grpc = require('grpc');
 let through2 = require('through2');
 let messages = require('./googleapis/google/assistant/embedded/v1alpha1/embedded_assistant_pb');
 let services = require('./googleapis/google/assistant/embedded/v1alpha1/embedded_assistant_grpc_pb');
-
-// Enums
-enum AssistantState {
-  IN_PROGRESS = 0,
-  FINISHED =  1
-}
-
-enum AssistantEvent {
-  END_OF_UTTERANCE = 1
-}
-
-// Interfaces
-interface AudioInOptions {
-  encoding: number
-  sampleRateHertz: number
-}
-
-interface AudioInConfig extends AudioInOptions {
-  setEncoding(encoding: number): void
-  setSampleRateHertz(encoding: number): void
-}
-
-interface AudioOutOptions extends AudioInOptions {
-  volumePercentage: number
-}
-
-interface AudioOutConfig extends AudioOutOptions, AudioInConfig {
-  setVolumePercentage(percentage: number): void
-}
-
-interface AssistantConfig {
-  input: AudioInOptions
-  output: AudioOutOptions
-}
-
-interface ConverseConfig {
-  setConverseState(state: Array<number>): void
-  setAudioInConfig(config: AudioInConfig): void
-  setAudioOutConfig(config: AudioOutConfig): void
-}
 
 // Impl
 class GoogleAssistantLogger {
@@ -74,7 +39,7 @@ class GoogleAssistantLogger {
 class GoogleAssistant {
   static ENDPOINT = 'embeddedassistant.googleapis.com'
 
-  private state: AssistantState
+  private state: State
   private service: any   // gRPC Service
   private converseConfig: ConverseConfig
   private conversationState: Array<number> | null
@@ -135,7 +100,7 @@ class GoogleAssistant {
     call.on('data', this._handleConversationState)
 
     call.write(request)
-    this.state = AssistantState.IN_PROGRESS;
+    this.state = State.IN_PROGRESS;
     return call;
   }
 
@@ -143,8 +108,8 @@ class GoogleAssistant {
     let response = res.toObject();
 
     // Handle end of user input
-    if(response.eventType == AssistantEvent.END_OF_UTTERANCE) {
-      this.state = AssistantState.FINISHED;
+    if(response.eventType == Event.END_OF_UTTERANCE) {
+      this.state = State.FINISHED;
     }
 
     // Handle continous conversations
